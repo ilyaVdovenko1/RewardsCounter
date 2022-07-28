ec95b5ed9f0231726d0b3e8af41cb5bd3ed7ffe5
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-07-28 23:55:10 +0300

    Added main domain models.

---
+++ RewardsCounter/RewardsCounter.Api/Domain/Reward.cs
@@ -0,0 +1,23 @@
+﻿namespace RewardsCounter.Api.Domain;
+
+/// <summary>
+/// Reward model.
+/// </summary>
+public class Reward
+{
+    /// <summary>
+    /// Gets customer that earn reward.
+    /// </summary>
+    /// <value>
+    /// Customer that earn reward.
+    /// </value>
+    public Customer Customer { get; init; }
+
+    /// <summary>
+    /// Gets total sum of points.
+    /// </summary>
+    /// <value>
+    /// Total sum of points.
+    /// </value>
+    public decimal PointsTotal { get; init; }
+}
---
+++ RewardsCounter/RewardsCounter.Api/Domain/Transaction.cs
@@ -0,0 +1,40 @@
+﻿namespace RewardsCounter.Api.Domain;
+
+/// <summary>
+/// Model of transaction.
+/// </summary>
+public record Transaction
+{
+    /// <summary>
+    /// Gets transaction id.
+    /// </summary>
+    /// <value>
+    /// Transaction id.
+    /// </value>
+    public Guid Id { get; init; }
+
+    /// <summary>
+    /// Gets time when transaction happened.
+    /// </summary>
+    /// <value>
+    /// Time when transaction happened.
+    /// </value>
+    public DateTime Timestamp { get; init; }
+
+    /// <summary>
+    /// Gets sum of transaction.
+    /// </summary>
+    /// <value>
+    /// Sum of transaction.
+    /// </value>
+    public decimal Sum { get; init; }
+
+    /// <summary>
+    /// Gets client id for current transaction.
+    /// </summary>
+    /// <value>
+    /// Client id for current transaction.
+    /// </value>
+    public Guid ClientId { get; init; }
+
+}
