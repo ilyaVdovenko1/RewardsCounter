c1f2e0c239235dede31e4d6327f0422bb973fc8a
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-07-29 00:14:03 +0300

    Added api controller.

---
+++ RewardsCounter/RewardsCounter.Api/Controllers/RewardsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RewardsCounter.Api.Controllers;
+
+/// <summary>
+/// This controller is access point for rewards count.
+/// </summary>
+[ApiController]
+[Route("api/[controller]")]
+public class RewardsController : ControllerBase
+{
+}
