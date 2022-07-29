09916b1d77e1b9f427643752d59d4be455a33101
ilyaVdovenko1 <ilyaforstudy1@gmail.com>
2022-07-29 17:12:56 +0300

    Added BLL in form of main service.

---
+++ RewardsCounter/RewardsCounter.Api/Services/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using RewardsCounter.Api.Repositories.Data;
+using RewardsCounter.Api.Services.Interfaces;
+using RewardsCounter.Api.Services.Models;
+
+namespace RewardsCounter.Api.Services;
+
+/// <summary>
+/// DI functionality for app service.
+/// </summary>
+public static class DependencyInjection
+{
+    /// <summary>
+    /// Injects default app service.
+    /// </summary>
+    /// <param name="serviceCollection">DI Service collection.</param>
+    /// <exception cref="ArgumentException">When can not find data context.</exception>
+    public static void AddDefaultAppService(this IServiceCollection serviceCollection)
+    {
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var context = serviceProvider.GetService<DataContext>();
+        if (context is null)
+        {
+            throw new ArgumentException("Can not recognize data context in service collection");
+        }
+
+        serviceCollection.AddSingleton<IAppService>(new DefaultAppService(context));
+    }
+}
---
+++ RewardsCounter/RewardsCounter.Api/Services/Models/DefaultAppService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RewardsCounter.Api.Domain;
+using RewardsCounter.Api.Exceptions;
+using RewardsCounter.Api.Repositories.Data;
+using RewardsCounter.Api.Services.Interfaces;
+
+namespace RewardsCounter.Api.Services.Models;
+
+/// <inheritdoc />
+public class DefaultAppService : IAppService
+{
+    private readonly DataContext dataContext;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DefaultAppService"/> class.
+    /// </summary>
+    /// <param name="dataContext">Db accessor.</param>
+    public DefaultAppService(DataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    /// <inheritdoc/>
+    public async Task<Customer> GetCustomer(Guid id)
+    {
+        var customerDal = await this.dataContext.Customers.FirstOrDefaultAsync(item => item.Id == id).ConfigureAwait(false);
+        if (customerDal is null)
+        {
+            throw new ClientNotFoundException($"Client with id {id} was not found.");
+        }
+
+        return new Customer()
+        {
+            CreationDate = customerDal.CreationDate,
+            Id = customerDal.Id,
+            Name = customerDal.Name,
+        };
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<Transaction>> GetTransactions(Guid clientId)
+    {
+        var transactions = await this.GetTransactions().ConfigureAwait(false);
+        return transactions.Where(item => item.ClientId == clientId);
+    }
+
+    private async Task<IEnumerable<Transaction>> GetTransactions()
+    {
+        var transactionsDal = await this.dataContext.Transactions.ToListAsync().ConfigureAwait(false);
+        var transactions = transactionsDal.Select(transactionDal => new Transaction()
+            {
+                ClientId = transactionDal.ClientId, Id = transactionDal.Id, Sum = transactionDal.Sum, Timestamp = transactionDal.Timestamp,
+            })
+            .ToList();
+        return transactions;
+    }
+}
